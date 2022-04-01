036a91ff24300574e333b52a62cad9271b44f07f
fanticheli <igor.fanticheli@hotmail.com>
2022-04-01 18:21:30 -0300

    Criando controller healthCheckController

--- src/controller/healthCheckController.js
+++ src/controller/healthCheckController.js
@@ -1,20 +1,22 @@
-var os = require("os");
-var ip = require("ip");
-var hostname = os.hostname();
+const os = require("os");
+const ip = require("ip");
+const hostname = os.hostname();
 
-const healthCheck = async (req, res) => {
-  try {
-    const response = {
-      message: "Estamos workando!!!",
-      hostname,
-      ip: ip.address(),
-    };
-    return res.status(200).json(response);
-  } catch (error) {
-    return res.status(400).json(error);
+class HealthCheckController {
+  contructor() {}
+
+  async healthCheck(req, res) {
+    try {
+      const response = {
+        message: "Estamos workando!!!",
+        hostname,
+        ip: ip.address(),
+      };
+      return res.status(200).json(response);
+    } catch (error) {
+      return res.status(400).json(error);
+    }
   }
-};
+}
 
-module.exports = {
-  healthCheck,
-};
+module.exports = new HealthCheckController();
--- src/routes/api/v1/healthCheckRoute.js
+++ src/routes/api/v1/healthCheckRoute.js
@@ -1,6 +1,6 @@
 const routes = require("express").Router();
-const HealthCheckController = require("../../../controller/healthCheckController");
+const healthCheckController = require("../../../controller/healthCheckController");
 
-routes.get("/", HealthCheckController.healthCheck);
+routes.get("/", healthCheckController.healthCheck);
 
 module.exports = routes;
