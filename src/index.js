ea6a59b29daf200dc33eccc8174954279bda7fe3
fanticheli <igor.fanticheli@hotmail.com>
2022-03-31 22:37:05 -0300

    Ajustando algumas configs

--- src/index.js
+++ src/index.js
@@ -6,17 +6,13 @@ const routes = require("./routes");
 const connectDb = require("./database/database");
 
 connectDb();
-
-// INICIANDO APP
 const app = express();
-const PORT = 3000;
+const port = process.env.PORT;
 
-//MIDDLEWARES
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-//GO!!!
-app.listen(PORT, () => {
-  console.log(`Estou online na porta ${PORT}!!!`);
+app.listen(port, () => {
+  console.log(`Estou online na porta ${port}!!!`);
 });
